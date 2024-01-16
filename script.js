a1de6eb41e25878b0fd8271685f80323683ecaa8
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-16 11:31:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
             // Get the radius from the user using the prompt() function
             let radius = prompt("Enter the radius of the circle:");
             // write you code here and display the result to the user
-           
+	 
+           function cal(radius) {
+           	return (Math.PI * Math.pow(radius, 2));
+           }
+			 let area = cal(radius);
+			 alert(area);
 }
 calculateArea();
