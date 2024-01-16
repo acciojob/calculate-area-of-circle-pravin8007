4dc3cb7963663a14f7d4f0da73e2eb8d8c0396a3
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-16 11:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,9 @@
             // write you code here and display the result to the user
 	 
            function cal(radius) {
-           	return (Math.PI * Math.pow(radius, 2));
+           	let area = (Math.PI * Math.pow(radius, 2));
+			   area = area.toFixed(2);
+			   return area;
            }
 			 let area = cal(radius);
 			 alert(area);
